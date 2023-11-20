b1e7ae39ff8274d31a89a5f6079cc0a145b53c68
leemirae <416homin@daum.net>
2023-11-20 17:55:13 +0900

    Feat : Connect naver-map-api

--- mern_client/src/styles/GlobalStyles.ts
+++ mern_client/src/styles/GlobalStyles.ts
@@ -3,28 +3,22 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
-
   a {
     text-decoration: none;
     color : black;
   }
-
   * {
     box-sizing: border-box;
   }
-
   html,body {
     height: 100%;
   }
-
   #root {
     height: 100%;
   }
-
   input:focus {
     outline : none;
   }
-
 `;
 
 export default GlobalStyles;
