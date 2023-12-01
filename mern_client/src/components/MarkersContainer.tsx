81d1f55483095a41f79d001526459d76ba878f90
leemirae <416homin@daum.net>
2023-12-01 15:25:16 +0900

    Feat : Add search-board

--- mern_client/src/components/MarkersContainer.tsx
+++ mern_client/src/components/MarkersContainer.tsx
@@ -1,16 +1,23 @@
 import { useAtom, useAtomValue } from "jotai";
-import React from "react";
+import React, { useCallback } from "react";
 import { mapAtom } from "../atoms/map";
 import { infosAtom, selectInfoAtom } from "../atoms/info";
-
 import Marker from "./common/Marker";
 import { Info } from "../types/info";
 import InfoWindow from "./common/infoWindow";
 
-function MarkersContainer() {
+interface MarkersContainerProps {
+  type?: "home" | "upload";
+}
+
+function MarkersContainer({ type = "home" }: MarkersContainerProps) {
   const map = useAtomValue(mapAtom);
   const infos = useAtomValue(infosAtom);
   const [selectInfo, setSelectInfo] = useAtom(selectInfoAtom);
+
+  const onSubmit = useCallback(() => {
+    console.log("제출!!");
+  }, []);
 
   if (!map || !infos) return null;
 
@@ -38,7 +45,11 @@ function MarkersContainer() {
           }}
         />
       )}
-      <InfoWindow map={map} selectInfo={selectInfo} />
+      <InfoWindow
+        map={map}
+        selectInfo={selectInfo}
+        onSubmit={type === "upload" ? onSubmit : undefined}
+      />
     </>
   );
 }
